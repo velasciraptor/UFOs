42e057e6844630dcf903c5c3be438825f007cbec
velasciraptor <amyvelasquez432@gmail.com>
2021-11-04 18:52:53 -0500

    still debugging filters with ask BCS help

--- static/js/app.js
+++ static/js/app.js
@@ -50,7 +50,7 @@ function updateFilters() {
     // 6. Call function to apply all filters and rebuild the table
   filterTable();
   
-  }
+}
   
   // 7. Use this function to filter the table when data is entered.
 function filterTable() {
